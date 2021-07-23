234fadfcb553238c885bf317cdd5527e9901d2dc
Elias Helander <eliashelander1@gmail.com>
2021-07-23 10:39:05 +0200

    added search

--- client/src/components/listings/Search.js
+++ client/src/components/listings/Search.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { ListingsContext } from '../../context/ListingsContext';
+import useDebounce from './useDebounce';
 
 const Search = () => {
+    const { setListings } = useContext(ListingsContext);
     const [searchValue, setSearchValue] = useState('');
+
+    const debounceValue = useDebounce(searchValue, 150);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setSearchValue('');
+
     };
+
+    useEffect(() => {
+        const fetchListings = async (searchTerm) => {
+            const res = await axios.get(`http://localhost:5000/api/listings?search=${searchTerm}`);
+            setListings(res.data);
+            setSearchValue(debounceValue);
+        }
+        fetchListings(debounceValue);
+    }, [debounceValue, setListings]);
 
     return (
         <form onSubmit={handleSubmit}>
