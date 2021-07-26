3ccbbeb422f674e1a7e864fb6a6492cbbedf9ab7
Jenny Svensson <hello@jennysvensson.com>
2021-07-26 17:58:40 +0200

    Listings styling

--- client/src/components/listings/Listings.js
+++ client/src/components/listings/Listings.js
@@ -29,13 +29,18 @@ const Listings = () => {
                 {listings
                     ?
                     listings.map(listing => (
-                        <div key={listing.l_id} >
+                        <div key={listing.l_id} className='listing'>
+                            {console.log(listing)}
+                            {listing?.details?.images.length > 0 &&
+                                <div
+                                    className='listing__image-container'
+                                    style={{
+                                        backgroundImage: `url("${listing?.details?.images[0]}")`
+                                    }}></div>
+                            }
                             <h2>{listing.title}</h2>
                             <p>{listing?.price?.day} kr</p>
                             <p>{listing.category}</p>
-                            {listing?.details?.images.length > 0 &&
-                                <img src={listing?.details?.images[0]} alt={listing.title} width="150px" />
-                            }
                             <Link to={`/listings/${listing.l_id}`} ><button className='button'>Details</button></Link>
                         </div>
                     ))
