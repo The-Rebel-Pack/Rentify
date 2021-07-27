0001d791d705c41d424fcdd642caa56b405f7a1b
Jenny Svensson <hello@jennysvensson.com>
2021-07-27 14:18:43 +0200

    Work in progress

--- api/utils/db_read.js
+++ api/utils/db_read.js
@@ -46,6 +46,8 @@ const findUserByEmail = async (email) => {
 const filterListingDetails = (res) => {
   return res.map(res => {
     res = res;
+    // res.updated_at = new Date(res.updated_at.replace(' ', 'T'));
+    console.log(res.updated_at);
     res.last_name = res.last_name[0];
     delete res.full_name;
     return res;
