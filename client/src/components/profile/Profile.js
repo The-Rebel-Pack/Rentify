5c1454641831415087299dfc2783fda71275fde2
Jenny Svensson <hello@jennysvensson.com>
2021-07-28 15:20:46 +0200

    Profile fix

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -26,21 +26,26 @@ const Profile = () => {
         }
     }, [token, fetchData]);
 
+    const returnFirstLetter = (word) => {
+        return word[0];
+    }
+
     return (
         <section className='app__section'>
             {data && data.map((contact) => (
                 <>
                     <h2 className='section__title'>Hi {contact.first_name}</h2>
-                    <h3>Your listings</h3>
+                    <h3 className='section__subtitle'>Would you like to create a new listing?</h3>
                     <p><Link to={`/listings/create`} ><button className='button'>Create new listing</button></Link></p>
-                    <h3>These are your current active listings</h3>
+                    <h3 className='section__subtitle'>These are your current active listings</h3>
                     <MyListings />
                     <div className='profile-details'>
                         <h3>Your personal details</h3>
                         <div key={contact.u_id}>
                             <p>Name: {contact.full_name}</p>
                             <p>Email: {contact.email}</p>
-                            <p>Public Contact Information</p>
+                            <h3 className='section__subtitle'>Public contact information for your listings</h3>
+                            <p>Name: {contact.first_name} {returnFirstLetter(contact.last_name)}</p>
                             {contact.u_details?.email
                                 && <p>Email: {contact.u_details?.email}</p>}
                             {contact.u_details?.phone
