f4547f23d85ebfbb0852509629e80398a5c54dff
Elias Helander <eliashelander1@gmail.com>
2021-07-28 14:38:04 +0200

    Added footer and fixed class to className in detailedListings.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './components/footer/Footer';
 import Nav from './components/navigation/Nav';
 
 const App = () => {
   return (
     <main className="app__main">
       <Nav />
+      <Footer />
     </main>
   );
 }
