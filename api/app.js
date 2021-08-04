e2a83982f0b12021658f666f9a8f0981b2ec057f
Elias Helander <eliashelander1@gmail.com>
2021-08-04 20:48:08 +0200

    removed comment

--- api/app.js
+++ api/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const morgan = require('morgan')
 const cors = require('cors')
 const middleware = require('./middleware');
 
@@ -8,7 +7,6 @@ const port = process.env.PORT || 5000;
 const { resetDb } = require('./utils/db_create');
 const { createHttpError } = require('./utils/validation');
 
-// app.use(morgan('dev'))
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
