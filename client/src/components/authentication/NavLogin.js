146c96fc0b98d47b0503689c86f251753786c327
Elias Helander <eliashelander1@gmail.com>
2021-09-10 15:11:02 +0200

    formatting

--- client/src/components/authentication/NavLogin.js
+++ client/src/components/authentication/NavLogin.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useContext } from 'react';
-import axios from 'axios';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import { AuthContext } from '../../context/AuthContext';
+import React, { useEffect, useContext } from "react";
+import axios from "axios";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { AuthContext } from "../../context/AuthContext";
 import { Link } from "react-router-dom";
 
 const Login = () => {
@@ -12,57 +12,57 @@ const Login = () => {
     firebase.auth().onAuthStateChanged((userCred) => {
       if (userCred) {
         setAuth(true);
-        window.localStorage.setItem('auth', 'true');
-        userCred.getIdToken()
-          .then((token) => {
-            setToken(token);
-          });
-      };
+        window.localStorage.setItem("auth", "true");
+        userCred.getIdToken().then((token) => {
+          setToken(token);
+        });
+      }
     });
   }, [setAuth, setToken]);
 
   const loginWithGoogle = () => {
-    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
-      .then( async (userCred) => {
+    firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(async (userCred) => {
         if (userCred) {
           setAuth(true);
-          window.localStorage.setItem('auth', 'true');
+          window.localStorage.setItem("auth", "true");
           await axios({
-            method: 'POST',
+            method: "POST",
             url: `${process.env.REACT_APP_API_BASE_URL}/api/users`,
             data: {
-              "id": userCred.user.uid,
-              "email": userCred.user.email,
-              "name": userCred.user.displayName
+              id: userCred.user.uid,
+              email: userCred.user.email,
+              name: userCred.user.displayName,
             },
           });
         }
-      }
-      )
+      })
       .catch((err) => {
         console.log(err);
-      })
+      });
   };
 
   const logout = () => {
     setAuth(false);
-    window.localStorage.removeItem('auth');
+    window.localStorage.removeItem("auth");
     firebase.auth().signOut();
   };
 
   return (
     <div>
       {auth ? (
-        <Link to="/" onClick={logout} >
+        <Link to="/" onClick={logout}>
           Logout
         </Link>
       ) : (
-        <Link to="/" onClick={loginWithGoogle} >
+        <Link to="/" onClick={loginWithGoogle}>
           Login
         </Link>
       )}
     </div>
   );
-}
+};
 
 export default Login;
