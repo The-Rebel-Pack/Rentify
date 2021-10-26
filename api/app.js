294d6627d31ef96f6b806b702bca27a47c868f88
Jenny <jenny@youwza.com>
2021-10-26 10:43:00 +0200

    Update syntax

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { resetDb } = require('./utils/db_create');
 const { createHttpError } = require('./utils/validation');
-const logger = require('./middleware/logger')
+const logger = require('./middleware/logger');
 
 app.use(cors());
 app.use(express.json());
@@ -28,14 +28,14 @@ app.use('/api/users', require('./routers/users'));
 
 app.use('/api/listings', require('./routers/listings'));
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createHttpError(404, 'Route not found'));
 });
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.log(err)
+  console.log(err);
   res.status(err.status || 500);
   res.send(err.message || 'Oops, something went wrong');
 });
